0c2003d5ebf2a307cb84343c405c81a44cf10d50
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-03-15 13:21:31 -0300

    changed  callback -> promises

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import "./models/database.js"
+import { getAllClientes } from "./models/database.js";
 const app = express();
 app.use(express.json()); 
 
@@ -30,8 +31,8 @@ app.get("/", (req, res) => {
     res.status(200).send("Node.js APP");
 });
 
-app.get("/clientes", (req, res) => {
-    res.status(200).json(clientes);
+app.get("/clientes", async (req, res) => {
+    res.status(200).json(await getAllClientes());
 });
 
 app.get("/clientes/:id" , (req, res) => {
@@ -57,3 +58,21 @@ app.delete("/clientes/:id", (req, res) =>{
 });
 
 export default app;
+
+/*
+RESTful API Development: Express is excellent for creating RESTful APIs. 
+You can define routes, handle requests, and manage URL structures efficiently. 
+For instance, you can build an API to manage user data, products, or any other resource.
+Middleware Functions: Middleware functions play a crucial role in Express. 
+They enable tasks like request parsing, logging, and authentication. 
+You can create custom middleware to handle specific tasks in your application.
+Template Engines: Express supports template engines like Pug or EJS. 
+These engines allow you to generate dynamic HTML content on the server side, which is useful for server-side rendering, SEO, and performance optimization.
+Database Integration: You can integrate databases (such as MongoDB, MySQL, or PostgreSQL) with Express. 
+Create routes to handle database queries, insertions, and updates.
+Authentication and Authorization: Implement user authentication and authorization using Express middleware. 
+You can use libraries like Passport.js for authentication strategies.
+Optimizing Performance: Express provides tools for optimizing performance, such as caching, compression, and minimizing response times.
+Remember that Express is versatile, unopinionated, and minimalistic, making it a popular choice for backend development in Node.js. 
+Explore these examples to enhance your Express.js skills and elevate your Node.js journey! 🚀
+*/
--- models/database.js
+++ models/database.js
@@ -1,6 +1,6 @@
 // Get the client
-//import mysql from 'mysql2/promise';
-import mysql from 'mysql2'
+import mysql from 'mysql2/promise';
+//import mysql from 'mysql2'
 // Create the connection to database
 const connection = await mysql.createConnection({
   host: 'localhost',
@@ -19,37 +19,47 @@ connection.query(dropTable, (err, results) => {
 });
 */
 
+async function createTableClientes(){
+  try {
+    const [results, fields] = await connection.query(
+      'CREATE TABLE IF NOT EXISTS clientes ' + 
+      '(id INT PRIMARY KEY AUTO_INCREMENT, nome TEXT, sobrenome TEXT, email TEXT, idade INT);'
+    );
+  
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  } catch (err) {
+    console.log(err);
+  }  
+}
 
-const createTableQuery = `CREATE TABLE IF NOT EXISTS clientes (id INT PRIMARY KEY AUTO_INCREMENT, nome TEXT, sobrenome TEXT, email TEXT, idade INT);`;
-connection.query(createTableQuery, (err, results) => {
-    if (err) {
-        console.error('Error creating table:', err);
-    } else {
-        console.log('Table created successfully:', results);
-    }
-});
-
-
-const insert = `INSERT INTO clientes (nome, sobrenome, email, idade) VALUES("José Da Picareta", "Ozvaldo", "pickaxeGuy@diamond.com", 22);`;
-connection.query(insert, (err, results) => {
-    if (err) {
-        console.error('Error inserting into table:', err);
-    } else {
-        console.log('Inserted into table successfully:', results);
-    }
-});
+export async function instertIntoTable(){
+  try {
+    const [results, fields] = await connection.query(
+      'INSERT INTO clientes (nome, sobrenome, email, idade) VALUES("Germano Da Picareta", "Ozvaldo", "pickaxeGuy@diamond.com", 22);'
+    );
+  
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  } catch (err) {
+    console.log(err);
+  }  
+}
 
 export async function getAllClientes(){
-  const select = `SELECT * FROM clientes;`;
-  connection.query(select, (err, results) => {
-    if (err) {
-        console.error('Error selecting from table cliente: ', err);
-    } else {
-        console.log('Selected table successfully:', results);
-        return results;
-    }
-  });
+  try {
+    const [results, fields] = await connection.query(
+      'SELECT * FROM clientes;'
+    );
+    
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+    return results;
+  } catch (err) {
+    console.log(err);
+  }  
 }
+
 /*
 // A simple SELECT query
 try {
