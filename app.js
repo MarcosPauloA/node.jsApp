39b722c999b63c410c0031b966ae09bf707dcc34
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-03-25 16:55:27 -0300

    removed excess no longer necessary

--- app.js
+++ app.js
@@ -1,69 +1,10 @@
 import express from "express";
-import "./models/database.js";
-import { getAllClientes, getClienteById } from "./models/database.js";
+import "./models/clientesTransactions.js";
+import { getAllClientes, getClienteById } from "./models/clientesTransactions.js";
 import routes from "./routes/index.js";
 const app = express();
 routes(app);
 //app.use(express.json()); 
-
-const clientes = [
-    {
-        id: 1,
-        nome: "Zé Da Picareta",
-        sobrenome: "Ozvaldo",
-        email: "pickaxeGuy@diamond.com",
-        idade: 22
-    },
-    {
-        id: 2,
-        nome: "José Do Machado",
-        sobrenome: "Carvalho",
-        email: "axeGuy@diamond.com",
-        idade: 33
-    },
-]
-
-function buscaCliente(id){
-    return clientes.findIndex(clientes => {
-        return clientes.id === Number(id);
-    })
-}
-
-app.get("/", (req, res) => {
-    res.status(200).send("nodeJs APP");
-});
-
-/*
-app.get("/clientes", async (req, res) => {
-    const listaClientes = await getAllClientes(); 
-    res.status(200).json(listaClientes);
-});
-
-
-app.get("/clientes/:id" , (req, res) => {
-    const index = buscaCliente(req.params.id);
-    res.status(200).json(clientes[index]);
-});
-app.put("/clientes/:id", (req, res) => {
-    const index = buscaCliente(req.params.id);
-    clientes[index].nome = req.body.nome;
-    res.status(200).json(clientes);
-    
-});
-   app.delete("/clientes/:id", (req, res) =>{
-    const index = buscaCliente(req.params.id);
-    clientes.splice(index, 1);
-    res.status(200).send("Cliente removido com sucesso!")
-});
-app.post("/clientes", (req, res) => {
-    clientes.push(req.body);
-    res.status(201).send("Cliente cadastrado com sucesso!");
-});
-*/
-
-
-
-
 
 export default app;
 
