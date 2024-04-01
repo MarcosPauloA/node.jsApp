3383ad704b399c576fd34c3cabcf111173979198
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-04-01 20:07:22 -0300

    added dotenv config and changed server.js to www.js

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -1,11 +1,13 @@
 // Get the client
 import mysql from 'mysql2/promise';
-
+// dotenv for config.env
+import dotenv from 'dotenv';
+dotenv.config();
 // Create the connection to database
 const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'nodeAppDatabase',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DB,
 });
 
 export default connection;
