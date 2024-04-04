9056bada39aa01b95dd7b7403b7a785548b9cda9
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-04-04 14:18:09 -0300

    Added main html page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const clientes = require('./clientesRoutes.js');
 const produtos = require('./produtosRoutes.js');
+const path = require('path');
 
 const routes = (app) => {
-    app.route('/').get((req, res) => res.status(200).send('NODE JS APP'));
+    app.route('/').get((req, res) => res.status(200).sendFile(
+        path.join(__dirname, '../views/index.html')));
 
     app.use(express.json(), clientes);
 
